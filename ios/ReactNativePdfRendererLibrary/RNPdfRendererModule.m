7e74faebf3410cbaffbd7d81434bb9cc97e74394
Douglas Nassif Roma Junior <douglas.junior@db1.com.br>
2023-03-09 14:33:04 -0300

    Optimize iOS PDFViewPageChangedNotification

--- ios/ReactNativePdfRendererLibrary/RNPdfRendererModule.m
+++ ios/ReactNativePdfRendererLibrary/RNPdfRendererModule.m
@@ -33,80 +33,85 @@ NSMutableDictionary *maxScaleFactors;
 
 - (UIView *)view
 {
-  if (maxScaleFactors == nil) {
-    maxScaleFactors = [[NSMutableDictionary alloc] init];
-  }
-
-  RNPDFView *view = [[RNPDFView alloc] init];
-  view.tag = [[NSDate new] timeIntervalSince1970];
-  [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(handlePageChange:) name:PDFViewPageChangedNotification object:nil];
-  return view;
+    if (maxScaleFactors == nil) {
+        maxScaleFactors = [[NSMutableDictionary alloc] init];
+        [NSNotificationCenter.defaultCenter addObserver:self selector:@selector(handlePageChange:) name:PDFViewPageChangedNotification object:nil];
+    }
+    
+    RNPDFView *view = [[RNPDFView alloc] init];
+    view.tag = [[NSDate new] timeIntervalSince1970];
+        
+    return view;
 }
 
 - (void)handlePageChange:(NSNotification*) notification {
-  RNPDFView *view = notification.object;
-  
-  NSUInteger currentPageNumber = [view.document indexForPage:view.currentPage];
-  
-  view.onPageChange(@{
-    @"position": [NSNumber numberWithInteger:currentPageNumber],
-    @"total":  [NSNumber numberWithInteger:view.document.pageCount],
-  });
+    if ([RNPDFView class] != [notification.object class]) {
+        return;
+    }
+    
+    RNPDFView *view = notification.object;
+    
+    NSUInteger currentPageNumber = [view.document indexForPage:view.currentPage];
+    
+    view.onPageChange(@{
+        @"position": [NSNumber numberWithInteger:currentPageNumber],
+        @"total":  [NSNumber numberWithInteger:view.document.pageCount],
+    });
 }
 
 RCT_EXPORT_VIEW_PROPERTY(onPageChange, RCTBubblingEventBlock)
 
 RCT_CUSTOM_VIEW_PROPERTY(source, NSString, RNPDFView)
 {
-  if (json != nil) {
-    NSURL *url = [NSURL URLWithString:json];
-    PDFDocument *pdfDocument = [[PDFDocument alloc] initWithURL:url];
-    
-    view.autoScales = YES;
-    view.displayDirection = kPDFDisplayDirectionVertical;
-
-    view.displayMode = kPDFDisplaySinglePageContinuous;
-    view.document = pdfDocument;
-  } else {
-    view.document = nil;
-  }
-  
-  dispatch_async(dispatch_get_main_queue(), ^{
-    NSString *viewTag =  [NSString stringWithFormat:@"%ld", (long)view.tag];
-
-    view.minScaleFactor = view.scaleFactorForSizeToFit;
-    
-    NSNumber *maxZoom = [maxScaleFactors valueForKey:viewTag];
-    [maxScaleFactors removeObjectForKey:viewTag];
-    
-    if ([maxZoom floatValue] > 0) {
-      view.maxScaleFactor = view.scaleFactorForSizeToFit * [maxZoom floatValue];
+    if (json != nil) {
+        NSURL *url = [NSURL URLWithString:json];
+        PDFDocument *pdfDocument = [[PDFDocument alloc] initWithURL:url];
+        
+        view.autoScales = YES;
+        view.displayDirection = kPDFDisplayDirectionVertical;
+        
+        view.displayMode = kPDFDisplaySinglePageContinuous;
+        view.document = pdfDocument;
+    } else {
+        view.document = nil;
     }
-  });
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        NSString *viewTag =  [NSString stringWithFormat:@"%ld", (long)view.tag];
+        
+        view.minScaleFactor = view.scaleFactorForSizeToFit;
+        
+        NSNumber *maxZoom = [maxScaleFactors valueForKey:viewTag];
+        [maxScaleFactors removeObjectForKey:viewTag];
+        
+        if ([maxZoom floatValue] > 0) {
+            view.maxScaleFactor = view.scaleFactorForSizeToFit * [maxZoom floatValue];
+        }
+    });
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(maxZoom, NSNumber, RNPDFView)
 {
-  NSString *viewTag =  [NSString stringWithFormat:@"%ld", (long)view.tag];
-  
-  dispatch_async(dispatch_get_main_queue(), ^{
-    if (json != nil) {
-      if (view.document != nil) {
-        view.maxScaleFactor = view.scaleFactorForSizeToFit * [json floatValue];
-      } else {
-        [maxScaleFactors setValue:json forKey: viewTag];
-      }
-    } else {
-      view.maxScaleFactor = 0;
-      [maxScaleFactors removeObjectForKey: viewTag];
-    }
-  });
+    NSString *viewTag =  [NSString stringWithFormat:@"%ld", (long)view.tag];
+    
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (json != nil) {
+            if (view.document != nil) {
+                view.maxScaleFactor = view.scaleFactorForSizeToFit * [json floatValue];
+            } else {
+                [maxScaleFactors setValue:json forKey: viewTag];
+            }
+        } else {
+            view.maxScaleFactor = 0;
+            [maxScaleFactors removeObjectForKey: viewTag];
+        }
+    });
 }
 
 RCT_CUSTOM_VIEW_PROPERTY(distanceBetweenPages, NSNumber, RNPDFView)
 {
-  view.pageBreakMargins = UIEdgeInsetsMake(0, 0, [json floatValue], 0);
-  [view setNeedsLayout];
+    view.pageBreakMargins = UIEdgeInsetsMake(0, 0, [json floatValue], 0);
+    [view setNeedsLayout];
 }
 
 @end
