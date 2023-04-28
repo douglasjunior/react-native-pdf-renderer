a90eedf55847aec6f5e7bfe7916a3694231535ca
Douglas Nassif Roma Junior <douglas.junior@db1.com.br>
2023-04-28 08:42:37 -0300

    Bump version 1.0.5

--- Sample/install.sh
+++ Sample/install.sh
@@ -6,6 +6,7 @@ echo "$unameOut";
 
 rm -rf ../node_modules/
 rm -rf node_modules/
+rm -rf ios/Pods/
 yarn install
 rm -rf node_modules/react-native-pdf-renderer/Sample/ 
 rm -rf node_modules/react-native-pdf-renderer/.git/
